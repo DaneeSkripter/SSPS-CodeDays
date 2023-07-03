2c9190abb8f488884f7b6ca19573112657d0fda1
daneeskripter <imdanee@seznam.cz>
2023-07-03 11:23:28 +0200

    objekty

---
+++ 01 - Úvod do témat, základy JavaScriptu (JS)/index.js
@@ -0,0 +1,16 @@
+const student = {
+    jmeno: "Radko",
+    prijmeni: "Sablik",
+    trida: "1.A",
+    aktivniStudent: true
+}
+
+const metadata = {
+    version: 1,
+    created: "2020-09-01"
+}
+
+console.log(student.jmeno)
+console.log(student.prijmeni)
+console.log(student.trida)
+console.log(student.aktivniStudent)

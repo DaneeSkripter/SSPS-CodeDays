67885a13720555555493c453ebdf817b36a0cce8
daneeskripter <imdanee@seznam.cz>
2023-07-03 12:20:17 +0200

    idk

--- 01 - Úvod do témat, základy JavaScriptu (JS)/index.js
+++ 01 - Úvod do témat, základy JavaScriptu (JS)/index.js
@@ -2,15 +2,15 @@ const student = {
     jmeno: "Radko",
     prijmeni: "Sablik",
     trida: "1.A",
-    aktivniStudent: true
-}
-
-const metadata = {
-    version: 1,
-    created: "2020-09-01"
+    aktivniStudent: true,
+    metadata: {
+        version: 1,
+        created: "2020-09-01"
+    }
 }
 
 console.log(student.jmeno)
 console.log(student.prijmeni)
 console.log(student.trida)
 console.log(student.aktivniStudent)
+console.log(student.metadata)
